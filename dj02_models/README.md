89551cce637fbb5d436bfcf37e9e39f0e41cd73c
Cooper <dancersoftaranto@gmail.com>
2022-12-20 22:53:11 +0300

    dj inclass models

---
+++ dj02_models/README.md
@@ -0,0 +1,27 @@
+Topics to be Covered:
+
+Django Intro Recap
+
+    Create virtual environment and activate
+    Install Django
+    Django-admin startproject
+    Python manage.py startapp
+    Make URL config of django project and apps
+
+What is Django Model
+
+    Model - View - template
+    What is models
+    What is ORM
+    Inheriting from model class
+    Model creation and what is the equivalent in database
+    Brief info about field types and field options
+    Create and explain str method for tables
+    Pyhton manage.py makemigrations
+    Pyhton manage.py migrate
+
+View tables in Admin Site -Register tables in admin.py
+
+    Create super user
+    Login admin site
+    View tables and add objects
---
+++ dj02_models/fscohort/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+#! models.py'deki her class database'de bir tablo'dur
+#* classlardaki her instance tablonun bir satırıdır.
+
+class Classname(models.Model):
+  pass # hiçbirşey yazılmayacaksa pass yazılır. Format bu
+
+class Student(models.Model):
+  first_name = models.CharField(max_length=50) #! field
+  last_name = models.CharField(max_length=50)
+  number = models.IntegerField(default=1111) # default değer atandı, yazılmazsa 1111 alır
+  about = models.TextField(blank=True, null=True) # boş bırakılabilir, null olabilir.
+  register = models.DateTimeField(auto_now_add=True) # auto_now_add : oluşturulan tarih/saati 1 kere alır
+  last_updated_date = models.DateTimeField(auto_now=True) # auto_now : güncellenen tarih/saati her güncellemede alır
+  is_active = models.BooleanField()
+  
+  def __str__(self):
+    return f"{self.number} + {self.first_name}"
+  
+  class Meta:
+    ordering = ["number"]
+    verbose_name_plural = "Student_list"
+    
+  
