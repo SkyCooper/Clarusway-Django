1fb6094e010b04b8278510477d7c77f6a50159b1
Cooper <dancersoftaranto@gmail.com>
2022-12-21 19:54:35 +0300

    post class models

--- dj02_models/fscohort/models.py
+++ dj02_models/fscohort/models.py
@@ -2,27 +2,40 @@ from django.db import models
 
 # Create your models here.
 #! models.py'deki her class database'de bir tablo'dur
-#* classlardaki her instance tablonun bir satırıdır.
+#* classlardaki her instance tablonun bir sütünudur..
 
 class Classname(models.Model):
   pass # hiçbirşey yazılmayacaksa pass yazılır. Format bu
 
-class Student(models.Model):
-  first_name = models.CharField(max_length=50) #! field
-  last_name = models.CharField(max_length=50)
+class Student(models.Model): #!Student isimli bir tablo oluştu db içinde
+  first_name = models.CharField(max_length=50) #! Tablonun sütunları, field'lar
+  last_name = models.CharField(max_length=50) #! içindekiler field option type
   number = models.IntegerField(default=1111) # default değer atandı, yazılmazsa 1111 alır
   about = models.TextField(blank=True, null=True) # boş bırakılabilir, null olabilir.
   register = models.DateTimeField(auto_now_add=True) # auto_now_add : oluşturulan tarih/saati 1 kere alır
   last_updated_date = models.DateTimeField(auto_now=True) # auto_now : güncellenen tarih/saati her güncellemede alır
   is_active = models.BooleanField()
-  
+                                                     # DateTimeField tarih/saat DateField sadece tarih
+  #? str medotu ile tablonun admin panelde nasıl görüneceğini ayarlıyoruz.
+  #? str medotunda yapılan değişikliş db değiştirmiyor, 
+  #? bunun için migrate komutlarını tekrar çalıştırmaya gerek kalmıyor. 
   def __str__(self):
-    return f"{self.number} + {self.first_name}"
+    return f"{self.number} {self.first_name}"
+  
+  #? indentation dikkat class içinde yazıyoruz Meta'yı.
+  #? ana classın bazı optionlarını buradan ayarlayabiliyoruz.
+  #? db'de değişiklik yapan bir option ise migrate komutları yeniden çalışması gerekli
+  #? alttakiler db'de değişiklik yapmıyor. (sadece görünüşü değiştiriyor.)
   
   class Meta:
-    ordering = ["number"]
-    verbose_name_plural = "Student_list"
-    
+    ordering = ["number"] #? ordering : numaraya göre sırala
+    verbose_name_plural = "Student_list" #? verbose_name_plural : Student olan isim Student_list oldu admi panelde.
+  
+  
+  #? method ekleme,
+  #? miras olarak alabilir, veya override edebiliriz.
+  #? en çok override edilen save() ve delete() metodlarıdır.
+  #? ilave bir method ekliyoruz,
   def student_year_status(self):
     "Returns the student's year status"
     import datetime
