fa5335159cfb5768f75b5493f25f24107e891d97
Cooper <dancersoftaranto@gmail.com>
2023-02-05 13:46:33 +0300

    blank/null true

--- dj02_models1/fscohort/models.py
+++ dj02_models1/fscohort/models.py
@@ -11,7 +11,14 @@ class Student(models.Model): #!Student isimli bir tablo oluştu db içinde
   first_name = models.CharField(max_length=50) #! Tablonun sütunları, field'lar
   last_name = models.CharField(max_length=50) #! içindekiler field option type
   number = models.IntegerField(default=1111) # default değer atandı, yazılmazsa 1111 alır
+  
   about = models.TextField(blank=True, null=True) # boş bırakılabilir, null olabilir.
+  #?------------- blank=True , null=True ----------------
+  # blank=True, serializer ile ilgilidir, yani boş bırakılabilir,
+  # null=True, DB ile ilgilidir, yani boş bırakılabilir ve DB null kayıt edilir,
+  #! eğer sadece  blank=True varsa veri boş gelebilir, ama DB kayıt edilmeden önce
+  #! bir işlem/hesaplama vs. yapılıp DB boş/null gitmesini önlemek gerekir.
+  
   register = models.DateTimeField(auto_now_add=True) # auto_now_add : oluşturulan tarih/saati 1 kere alır
   last_updated_date = models.DateTimeField(auto_now=True) # auto_now : güncellenen tarih/saati her güncellemede alır
   is_active = models.BooleanField()
