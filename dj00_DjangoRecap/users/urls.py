dff62f171420e16da3e33b800bf3b11ebac79fee
Cooper <dancersoftaranto@gmail.com>
2023-02-08 16:16:21 +0300

    dj_rest_auth

--- dj00_DjangoRecap/users/urls.py
+++ dj00_DjangoRecap/users/urls.py
@@ -1,7 +1,26 @@
 from django.urls import path,include
-from .views import RegisterView
+from .views import RegisterView, logout
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
-    path("register/", RegisterView.as_view())
+    #? dj_rest_auth, paketi içinde default url'ler mevcut;
+    #? bunu kullanınca ilave login/logout url tanımlamaya gerek yok.
+        # user/ auth/ password/reset/ [name='rest_password_reset']
+        # user/ auth/ password/reset/confirm/ [name='rest_password_reset_confirm']
+        # user/ auth/ login/ [name='rest_login']
+        # user/ auth/ logout/ [name='rest_logout']
+        # user/ auth/ user/ [name='rest_user_details']
+        # user/ auth/ password/change/ [name='rest_password_change']
+    
+    path("register/", RegisterView.as_view()),
+        # user/ register/
+    
+    path('login/', views.obtain_auth_token),
+    #? obtain_auth_token bir login endpoint  oluşturuyor. username ve password giriliyor,
+    #? db'de eğer bu user için bir token varsa onu getiriyor, yoksa yeni bir token create ediyor ve onu getiriyor.
+        # user/ login/
+    
+    path('logout/', logout),
+        # user/ logout/
 ]
--- dj00_DjangoRecap/users/views.py
+++ dj00_DjangoRecap/users/views.py
@@ -1,14 +1,23 @@
 from rest_framework.generics import CreateAPIView
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer
 
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+#! register için;
 class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-  
+    
+#! yeni bir user oluşturulduğunda onun için token oluşturması için yazılan metod;
+#! bu bize register olduktan sonra tekrar login sayfasına gitmeden login olmamızı sağlıyor.
+#! bunu da user create ediliğinde dönen Response içine key field'nı ekleyerek yapıyor.
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -18,3 +27,16 @@ class RegisterView(CreateAPIView):
         data["key"] = token.key
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+#? dj_rest_auth, paketi içinde default url'ler mevcut;
+#? bunu kullanınca ilave login/logout view tanımlamaya gerek yok.   
+
+#! login için;
+# ilave metod yazılmadan django'nun view içindeki obtain_auth_token kullandık.
+
+#! logout için;
+@api_view(['POST'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"message": 'User Logout: Token Deleted'})
