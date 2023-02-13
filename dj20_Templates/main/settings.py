952989d1966b50c00a5a3f675a498979e90fd6d8
Cooper <dancersoftaranto@gmail.com>
2023-02-13 22:37:38 +0300

    static

--- dj20_Templates/main/settings.py
+++ dj20_Templates/main/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#? static;
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
