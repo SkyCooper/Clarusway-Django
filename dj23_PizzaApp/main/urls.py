e997011122f8fcc982c6e88ccbfe8511ddf3efd1
Cooper <dancersoftaranto@gmail.com>
2023-02-18 11:34:40 +0300

    model

--- dj23_PizzaApp/main/urls.py
+++ dj23_PizzaApp/main/urls.py
@@ -6,3 +6,9 @@ urlpatterns = [
     path('', include('pizzas.urls')),
     path('users/', include('users.urls')),
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dj23_PizzaApp/pizzas/models.py
+++ dj23_PizzaApp/pizzas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    toppings = models.ManyToManyField(Topping)
+    image = models.ImageField(upload_to='pizza_pics', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+Size = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+)
+
+
+class Order(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=100, choices=Size, default="M")
+    quantity = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.user.username} ordered {self.pizza.name}"
